e7413d08ee4be2fa6701b164da93285bd5c0d8d6
Unknown <hemaxi.1996@gmail.com>
2022-05-08 11:01:47 +0100

    test with a smaller subset

--- cnn.py
+++ cnn.py
@@ -134,7 +134,7 @@ def main():
     configure_seed(seed=42)
     configure_device(opt.gpu_id)
 
-    _examples_ = [17111,2156,2163]
+    _examples_ = [100,100,100]
 
     print("Loading data...") ## input manual nexamples train, dev e test
     train_dataset = ECGImageDataset(opt.data, _examples_, 'train')
