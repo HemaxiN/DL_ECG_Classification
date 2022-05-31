ef7dc3d5fd465ce80b6fc7e4c4f3001b2d68263d
Unknown <hemaxi.1996@gmail.com>
2022-05-31 15:05:25 +0100

    simpler cnn based on the vggnet

--- vggnet.py
+++ vggnet.py
@@ -22,27 +22,19 @@ class VGG16(nn.Module):
         self.n_classes = n_classes
 
         n_filters = 16
-        self.conv1_1 = nn.Conv2d(in_channels=9, out_channels=n_filters, kernel_size=3, padding=1)
-        self.conv1_2 = nn.Conv2d(in_channels=n_filters, out_channels=n_filters, kernel_size=3, padding=1)
+        self.conv1_1 = nn.Conv2d(9, n_filters, 3, padding=1)
+        self.conv1_2 = nn.Conv2d(n_filters, n_filters, 3, padding=1)
 
-        self.conv2_1 = nn.Conv2d(in_channels=n_filters, out_channels=n_filters*2, kernel_size=3, padding=1)
-        self.conv2_2 = nn.Conv2d(in_channels=n_filters*2, out_channels=n_filters*2, kernel_size=3, padding=1)
+        self.conv2_1 = nn.Conv2d(n_filters, n_filters*2, 3, padding=1)
+        self.conv2_2 = nn.Conv2d(n_filters*2, n_filters*2, 3, padding=1)
 
-        self.conv3_1 = nn.Conv2d(in_channels=n_filters*2, out_channels=n_filters*4, kernel_size=3, padding=1)
-        self.conv3_2 = nn.Conv2d(in_channels=n_filters*4, out_channels=n_filters*4, kernel_size=3, padding=1)
-        self.conv3_3 = nn.Conv2d(in_channels=n_filters*4, out_channels=n_filters*4, kernel_size=3, padding=1)
+        self.conv3_1 = nn.Conv2d(n_filters*2, n_filters*4, 3, padding=1)
+        self.conv3_2 = nn.Conv2d(n_filters*4, n_filters*4, 3, padding=1)
+        self.conv3_3 = nn.Conv2d(n_filters*4, n_filters*4,3, padding=1)
 
-        self.conv4_1 = nn.Conv2d(in_channels=n_filters*4, out_channels=n_filters*8, kernel_size=3, padding=1)
-        self.conv4_2 = nn.Conv2d(in_channels=n_filters*8, out_channels=n_filters*8, kernel_size=3, padding=1)
-        self.conv4_3 = nn.Conv2d(in_channels=n_filters*8, out_channels=n_filters*8, kernel_size=3, padding=1)
+        self.maxpool = nn.MaxPool2d(2, stride=2)
 
-        self.conv5_1 = nn.Conv2d(in_channels=n_filters*8, out_channels=n_filters*8, kernel_size=3, padding=1)
-        self.conv5_2 = nn.Conv2d(in_channels=n_filters*8, out_channels=n_filters*8, kernel_size=3, padding=1)
-        self.conv5_3 = nn.Conv2d(in_channels=n_filters*8, out_channels=n_filters*8, kernel_size=3, padding=1)
-
-        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.fc1 = nn.Linear(8192, 4096)
+        self.fc1 = nn.Linear(65536, 4096)
         self.fc2 = nn.Linear(4096, 4096)
         self.fc3 = nn.Linear(4096, self.n_classes)
 
@@ -51,19 +43,12 @@ class VGG16(nn.Module):
         x = F.relu(self.conv1_2(x))
         x = self.maxpool(x)
         x = F.relu(self.conv2_1(x))
-        x = F.relu(self.conv2_2(x))
+        x = F.dropout(F.relu(self.conv2_2(x)),0.3)
         x = self.maxpool(x)
+        x = F.dropout(x,0.3)
         x = F.relu(self.conv3_1(x))
         x = F.relu(self.conv3_2(x))
-        x = F.relu(self.conv3_3(x))
-        x = self.maxpool(x)
-        x = F.relu(self.conv4_1(x))
-        x = F.relu(self.conv4_2(x))
-        x = F.relu(self.conv4_3(x))
-        x = self.maxpool(x)
-        x = F.relu(self.conv5_1(x))
-        x = F.relu(self.conv5_2(x))
-        x = F.relu(self.conv5_3(x))
+        x = F.dropout(F.relu(self.conv3_3(x)),0.3)
         x = self.maxpool(x)
         x = x.reshape(x.shape[0], -1)
         x = F.relu(self.fc1(x))
