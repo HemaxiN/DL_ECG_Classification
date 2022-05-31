a941903605096152ad3b408e0064772d571294f1
ricardo-santos-fhp <ricardo.santos@fraunhofer.pt>
2022-05-31 14:37:16 +0100

    joint fusion. updated early fusion with image reduction dense layer.

--- early_fusion.py
+++ early_fusion.py
@@ -26,7 +26,7 @@ from datetime import datetime
 import os
 
 
-class EarlyFusionDataset(Dataset):
+class FusionDataset(Dataset):
     def __init__(self, sig_path, img_path, train_dev_test, part='train'):
         self.sig_path = sig_path
         self.img_path = img_path
@@ -51,7 +51,8 @@ class EarlyFusionDataset(Dataset):
 
 
 class EarlyFusionNet(nn.Module):
-    def __init__(self, n_classes, n_features, hidden_size, dropout, sig_model, img_model, sig_hook, img_hook):
+    def __init__(self, n_classes, sig_features, img_features, hidden_size, dropout, sig_model, img_model,
+                 sig_hook, img_hook):
         """
         n_classes (int)
         n_features (int)
@@ -66,12 +67,14 @@ class EarlyFusionNet(nn.Module):
         self.sig_hook = sig_hook
         self.img_hook = img_hook
 
-        self.fc1 = nn.Linear(n_features, hidden_size * 2)
+        self.maxpool = nn.MaxPool2d(3, stride=3)
+        self.fc_img = nn.Linear(img_features, sig_features)
+
+        self.fc1 = nn.Linear(sig_features * 2, hidden_size * 2)
         self.fc2 = nn.Linear(hidden_size * 2, hidden_size)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(p=dropout)
         self.out = nn.Linear(hidden_size, n_classes)
-        self.maxpool = nn.MaxPool2d(3, stride=3)
 
     def forward(self, X_sig, X_img):
         """
@@ -84,8 +87,9 @@ class EarlyFusionNet(nn.Module):
         act_sig = activation[self.sig_hook][:, -1, :]
         act_img = activation[self.img_hook]
         flat_img = torch.flatten(self.maxpool(act_img), start_dim=1)
-        # flat_img = torch.flatten(act_img.mean(1), start_dim=1)
-        X = torch.cat((act_sig, flat_img), dim=1)
+
+        x_img = self.dropout(self.relu(self.fc_img(flat_img)))
+        X = torch.cat((act_sig, x_img), dim=1)
 
         X = self.dropout(self.relu(self.fc1(X)))
         X = self.dropout(self.relu(self.fc2(X)))
@@ -104,8 +108,8 @@ def get_activation(name):
     return hook
 
 
-def train_early_batch(X_sig, X_img, y, model, optimizer, criterion,
-                      gpu_id=None, **kwargs):
+def fusion_train_batch(X_sig, X_img, y, model, optimizer, criterion,
+                       gpu_id=None, **kwargs):
     """
     X (batch_size, 1000, 3): batch of examples
     y (batch_size, 4): ground truth labels_train
@@ -123,7 +127,7 @@ def train_early_batch(X_sig, X_img, y, model, optimizer, criterion,
     return loss.item()
 
 
-def early_predict(model, X_sig, X_img):
+def fusion_predict(model, X_sig, X_img):
     """
     Make labels_train predictions for "X" (batch_size, 1000, 3)
     """
@@ -133,7 +137,7 @@ def early_predict(model, X_sig, X_img):
     return pred_labels
 
 
-def early_evaluate(model, dataloader, part, gpu_id=None):
+def fusion_evaluate(model, dataloader, part, gpu_id=None):
     """
     model: Pytorch model
     X (batch_size, 1000, 3) : batch of examples
@@ -145,7 +149,7 @@ def early_evaluate(model, dataloader, part, gpu_id=None):
         for i, (X_sig_batch, X_img_batch, y_batch) in enumerate(dataloader):
             print('eval {} of {}'.format(i + 1, len(dataloader)), end='\r')
             X_sig_batch, X_img_batch, y_batch = X_sig_batch.to(gpu_id), X_img_batch.to(gpu_id), y_batch.to(gpu_id)
-            y_pred = early_predict(model, X_sig_batch, X_img_batch)
+            y_pred = fusion_predict(model, X_sig_batch, X_img_batch)
             y_true = np.array(y_batch.cpu())
             matrix = compute_scores(y_true, y_pred, matrix)
 
@@ -164,14 +168,14 @@ def early_evaluate(model, dataloader, part, gpu_id=None):
 
 
 # Validation loss
-def early_compute_loss(model, dataloader, criterion, gpu_id=None):
+def fusion_compute_loss(model, dataloader, criterion, gpu_id=None):
     model.eval()
     with torch.no_grad():
         val_losses = []
         for i, (X_sig_batch, X_img_batch, y_batch) in enumerate(dataloader):
             print('eval {} of {}'.format(i + 1, len(dataloader)), end='\r')
             X_sig_batch, X_img_batch, y_batch = X_sig_batch.to(gpu_id), X_img_batch.to(gpu_id), y_batch.to(gpu_id)
-            y_pred = early_predict(model, X_sig_batch, X_img_batch)
+            y_pred = fusion_predict(model, X_sig_batch, X_img_batch)
             loss = criterion(torch.Tensor(y_pred).to(gpu_id), y_batch)
             val_losses.append(loss.item())
             del X_sig_batch
@@ -189,7 +193,7 @@ def main():
     parser.add_argument('-image_data', default='Dataset/Images/', help="Path to the 2D image dataset.")
     parser.add_argument('-signal_model', default='gru', help="Description of the 1D ECG model.")
     parser.add_argument('-image_model', default='alexnet', help="Description of the 2D image model.")
-    parser.add_argument('-epochs', default=25, type=int, help="""Number of epochs to train the model.""")
+    parser.add_argument('-epochs', default=50, type=int, help="""Number of epochs to train the model.""")
     parser.add_argument('-batch_size', default=128, type=int, help="Size of training batch.")
     parser.add_argument('-learning_rate', type=float, default=0.01)
     parser.add_argument('-dropout', type=float, default=0.3)
@@ -197,7 +201,7 @@ def main():
     parser.add_argument('-optimizer', choices=['sgd', 'adam'], default='adam')
     parser.add_argument('-gpu_id', type=int, default=0)
     parser.add_argument('-path_save_model', default='save_models/', help='Path to save the model')
-    parser.add_argument('-hidden_size', type=int, default=128)
+    parser.add_argument('-hidden_size', type=int, default=256)
     opt = parser.parse_args()
     print(opt)
 
@@ -240,25 +244,25 @@ def main():
     img_model.eval()
 
     # REGISTER HOOKS
-    # TODO adapt for resnet - need to check sizes of images
-    img_hook = 'conv2d_3'
+    img_hook = 'conv2d_5'
     sig_hook = 'rnn'
-    img_model.conv2d_3.register_forward_hook(get_activation(img_hook))
+    img_model.conv2d_5.register_forward_hook(get_activation(img_hook))
     sig_model.rnn.register_forward_hook(get_activation(sig_hook))
 
     img_size = {'conv2d_1': 6400, 'conv2d_2': 3200, 'conv2d_3': 1024, 'conv2d_4': 2048, 'conv2d_5': 4096}
-    features_size = 128 + img_size[img_hook]
+    sig_features = 128
+    img_features = img_size[img_hook]
 
     # LOAD DATA
-    train_dataset = EarlyFusionDataset(opt.signal_data, opt.image_data, [17111, 2156, 2163], part='train')
-    dev_dataset = EarlyFusionDataset(opt.signal_data, opt.image_data, [17111, 2156, 2163], part='dev')
-    test_dataset = EarlyFusionDataset(opt.signal_data, opt.image_data, [17111, 2156, 2163], part='test')
+    train_dataset = FusionDataset(opt.signal_data, opt.image_data, [17111, 2156, 2163], part='train')
+    dev_dataset = FusionDataset(opt.signal_data, opt.image_data, [17111, 2156, 2163], part='dev')
+    test_dataset = FusionDataset(opt.signal_data, opt.image_data, [17111, 2156, 2163], part='test')
 
     train_dataloader = DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=False)
     dev_dataloader = DataLoader(dev_dataset, batch_size=opt.batch_size, shuffle=False)
     test_dataloader = DataLoader(test_dataset, batch_size=opt.batch_size, shuffle=False)
 
-    model = EarlyFusionNet(4, features_size, opt.hidden_size, opt.dropout,
+    model = EarlyFusionNet(4, sig_features, img_features, opt.hidden_size, opt.dropout,
                            sig_model, img_model, sig_hook, img_hook).to(opt.gpu_id)
 
     # get an optimizer
@@ -294,7 +298,7 @@ def main():
         # print(list(sig_model.rnn.parameters())[0][:10])
         for i, (X_sig_batch, X_img_batch, y_batch) in enumerate(train_dataloader):
             print('batch {} of {}'.format(i + 1, len(train_dataloader)), end='\r')
-            loss = train_early_batch(
+            loss = fusion_train_batch(
                 X_sig_batch, X_img_batch, y_batch, model, optimizer, criterion, gpu_id=opt.gpu_id)
             del X_sig_batch
             del X_img_batch
@@ -306,8 +310,8 @@ def main():
         print('Training loss: %.4f' % (mean_loss))
 
         train_mean_losses.append(mean_loss)
-        sensitivity, specificity = early_evaluate(model, dev_dataloader, 'dev', gpu_id=opt.gpu_id)
-        val_loss = early_compute_loss(model, dev_dataloader, criterion, gpu_id=opt.gpu_id)
+        sensitivity, specificity = fusion_evaluate(model, dev_dataloader, 'dev', gpu_id=opt.gpu_id)
+        val_loss = fusion_compute_loss(model, dev_dataloader, criterion, gpu_id=opt.gpu_id)
         valid_mean_losses.append(val_loss)
         valid_sensitivity.append(sensitivity)
         valid_specificity.append(specificity)
@@ -319,13 +323,10 @@ def main():
         # save the model at each epoch where the validation loss is the best so far
         if val_loss == np.min(valid_mean_losses):
             torch.save(model.state_dict(),
-                       os.path.join(opt.path_save_model, str(datetime.timestamp(dt)) + 'early_lr_3max_model' + str(e.item())))
-        elif sensitivity == np.max(valid_sensitivity):
-            torch.save(model.state_dict(),
-                       os.path.join(opt.path_save_model, str(datetime.timestamp(dt)) + 'early_lr_3max_model_val' + str(e.item())))
+                       os.path.join(opt.path_save_model, str(datetime.timestamp(dt)) + 'early_model' + str(e.item())))
 
     # Results on test set:
-    matrix = early_evaluate(model, test_dataloader, 'test', gpu_id=opt.gpu_id)
+    matrix = fusion_evaluate(model, test_dataloader, 'test', gpu_id=opt.gpu_id)
 
     # compute sensitivity and specificity for each class:
     MI_sensi = matrix[0, 0] / (matrix[0, 0] + matrix[0, 1])
@@ -350,13 +351,13 @@ def main():
 
     # plot
     plot_losses(epochs, valid_mean_losses, train_mean_losses, ylabel='Loss',
-                name='training-validation-loss-early3max-{}-{}-{}-{}-{}-{}-{}'.format(
+                name='training-validation-loss-early-{}-{}-{}-{}-{}-{}-{}'.format(
                     img_hook, opt.learning_rate, opt.optimizer, opt.dropout, opt.epochs, opt.hidden_size, opt.batch_size))
     plot(epochs, valid_specificity, ylabel='Specificity',
-         name='validation-specificity-early3max-{}-{}-{}-{}-{}-{}-{}'.format(
+         name='validation-specificity-early-{}-{}-{}-{}-{}-{}-{}'.format(
              img_hook, opt.learning_rate, opt.optimizer, opt.dropout, opt.epochs, opt.hidden_size, opt.batch_size))
     plot(epochs, valid_sensitivity, ylabel='Sensitivity',
-         name='validation-sensitivity-early3max-{}-{}-{}-{}-{}-{}-{}'.format(
+         name='validation-sensitivity-early-{}-{}-{}-{}-{}-{}-{}'.format(
              img_hook, opt.learning_rate, opt.optimizer, opt.dropout, opt.epochs, opt.hidden_size, opt.batch_size))
 
 
--- joint_fusion.py
+++ joint_fusion.py
@@ -13,109 +13,22 @@ from torch import nn
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
-from utils import configure_device, configure_seed, ECGImageDataset, Dataset_for_RNN, plot, plot_losses
+from utils import configure_device, configure_seed, ECGImageDataset, Dataset_for_RNN, plot, plot_losses, compute_scores, \
+    compute_scores_dev
 import gru as gru
 import numpy as np
+import statistics
 
 import AlexNet as alexnet
 import resnet as resnet
 
 from datetime import datetime
 import os
+import early_fusion as early
 
 
-def modalities_evaluate(model, dataloader, gpu_id=None):
-    """
-    model: Pytorch model
-    X (batch_size, 1000, 3) : batch of examples
-    y (batch_size,4): ground truth labels_train
-    """
-    model.eval()
-    with torch.no_grad():
-        pred_logger = []
-        y_logger = []
-        for i, (x_batch, y_batch) in enumerate(dataloader):
-            print('eval {} of {}'.format(i + 1, len(dataloader)), end='\r')
-            x_batch, y_batch = x_batch.to(gpu_id), y_batch.to(gpu_id)
-            y_pred = gru.predict(model, x_batch)
-            y_true = np.array(y_batch.cpu())
-
-            pred_logger.append(y_pred)
-            y_logger.append(y_true)
-
-            del x_batch
-            del y_batch
-            torch.cuda.empty_cache()
-
-        model.train()
-
-    return pred_logger, y_logger
-
-
-class LateFusionDataset(Dataset):
-    def __init__(self, sig_path, img_path, sig_model, img_model, sig_type, img_type,
-                 train_dev_test, gpu_id, batch_size, part='train'):
-        self.sig_path = sig_path
-        self.img_path = img_path
-        self.sig_model = sig_model
-        self.img_model = img_model
-        self.sig_type = sig_type
-        self.img_type = img_type
-        self.part = part
-        self.train_dev_test = train_dev_test
-        self.gpu_id = gpu_id
-        self.batch_size = batch_size
-
-        self.sig_dataset = Dataset_for_RNN(self.sig_path, self.train_dev_test, self.part)
-        self.img_dataset = ECGImageDataset(self.img_path, self.train_dev_test, self.part)
-
-        self.sig_dataloader = DataLoader(self.sig_dataset, batch_size=self.batch_size, shuffle=False)
-        self.img_dataloader = DataLoader(self.img_dataset, batch_size=self.batch_size, shuffle=False)
-
-        self.X = []
-        self.y = []
-
-        self.compute_preds()
-
-    def __len__(self):
-        if self.part == 'train':
-            return self.train_dev_test[0]
-        elif self.part == 'dev':
-            return self.train_dev_test[1]
-        elif self.part == 'test':
-            return self.train_dev_test[2]
-
-    def __getitem__(self, idx):
-        return torch.tensor(self.X[idx]).float(), torch.tensor(self.y[idx]).float()
-
-    def compute_preds(self):
-        '''
-        Loads data, performs prediction using the unimodal models and concatenates data.
-        '''
-        if self.sig_type == 'gru':
-            sig_pred, sig_y = modalities_evaluate(self.sig_model, self.sig_dataloader, gpu_id=self.gpu_id)
-        else:
-            raise ValueError('1D model is not defined.')
-
-        if self.img_type == 'alexnet':
-            img_pred, img_y = modalities_evaluate(self.img_model, self.img_dataloader, gpu_id=self.gpu_id)
-        elif self.img_type == 'resnet':
-            img_pred, img_y = modalities_evaluate(self.img_model, self.img_dataloader, gpu_id=self.gpu_id)
-        else:
-            raise ValueError('2D model is not defined.')
-
-        for i in range(len(sig_pred)):
-
-            if len(self.X) == 0:
-                self.X = np.c_[sig_pred[i], img_pred[i]]
-                self.y = sig_y[i]
-            else:
-                self.X = np.r_[self.X, np.c_[sig_pred[i], img_pred[i]]]
-                self.y = np.r_[self.y, sig_y[i]]
-
-
-class LateFusionNet(nn.Module):
-    def __init__(self, n_classes, n_features, hidden_size, dropout):
+class JointFusionNet(nn.Module):
+    def __init__(self, n_classes, sig_features, img_features, hidden_size, dropout, sig_model, img_model):
         """
         n_classes (int)
         n_features (int)
@@ -123,21 +36,43 @@ class LateFusionNet(nn.Module):
         activation_type (str)
         dropout (float): dropout probability
         """
-        super(LateFusionNet, self).__init__()
+        super(JointFusionNet, self).__init__()
 
-        self.hidden = nn.Linear(n_features, hidden_size)
-        self.activation = nn.ReLU()
+        self.sig_model = sig_model
+        self.img_model = img_model
+
+        self.fc_img = nn.Linear(img_features, sig_features)
+
+        self.fc1 = nn.Linear(sig_features * 2, hidden_size * 2)
+        self.fc2 = nn.Linear(hidden_size * 2, hidden_size)
+        self.relu = nn.ReLU()
         self.dropout = nn.Dropout(p=dropout)
         self.out = nn.Linear(hidden_size, n_classes)
 
-    def forward(self, x):
+    def forward(self, X_sig, X_img):
         """
         x (batch_size x n_features): a batch of training examples
         """
-        x = self.activation(self.hidden(x))
-        x = self.dropout(x)
-        x = self.out(x)
 
+        sig_out = self.sig_model(X_sig)
+        img_out = self.img_model(X_img)
+
+        x_img = self.dropout(self.relu(self.fc_img(img_out)))
+
+        X = torch.cat((sig_out, x_img), dim=1)
+
+        X = self.dropout(self.relu(self.fc1(X)))
+        X = self.dropout(self.relu(self.fc2(X)))
+        X = self.out(X)
+
+        return X
+
+
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+
+    def forward(self, x):
         return x
 
 
@@ -147,15 +82,15 @@ def main():
     parser.add_argument('-image_data', default='Dataset/Images/', help="Path to the 2D image dataset.")
     parser.add_argument('-signal_model', default='gru', help="Description of the 1D ECG model.")
     parser.add_argument('-image_model', default='alexnet', help="Description of the 2D image model.")
-    parser.add_argument('-epochs', default=50, type=int, help="""Number of epochs to train the model.""")
-    parser.add_argument('-batch_size', default=256, type=int, help="Size of training batch.")
+    parser.add_argument('-epochs', default=25, type=int, help="""Number of epochs to train the model.""")
+    parser.add_argument('-batch_size', default=64, type=int, help="Size of training batch.")
     parser.add_argument('-learning_rate', type=float, default=0.01)
     parser.add_argument('-dropout', type=float, default=0.3)
     parser.add_argument('-l2_decay', type=float, default=0)
     parser.add_argument('-optimizer', choices=['sgd', 'adam'], default='adam')
     parser.add_argument('-gpu_id', type=int, default=0)
     parser.add_argument('-path_save_model', default='save_models/', help='Path to save the model')
-    parser.add_argument('-hidden_size', type=int, default=64)
+    parser.add_argument('-hidden_size', type=int, default=128)
     opt = parser.parse_args()
     print(opt)
 
@@ -190,24 +125,26 @@ def main():
         raise ValueError('2D model is not defined.')
 
     sig_model.load_state_dict(torch.load(sig_path, map_location=torch.device(opt.gpu_id)))
-    sig_model.eval()
-
     img_model.load_state_dict(torch.load(img_path, map_location=torch.device(opt.gpu_id)))
-    img_model.eval()
+
+    # REPLACE UNWANTED LAYERS TO BE IGNORED WITH IDENTITY FUNCTION
+    sig_model.fc = Identity()
+    img_model.linear_3 = Identity()  # applied on the last dense layer only
+
+    sig_features = 128
+    img_features = 2048  # 9216, 4096, 2048
 
     # LOAD DATA
-    train_dataset = LateFusionDataset(opt.signal_data, opt.image_data, sig_model, img_model, sig_type, img_type,
-                                      [17111, 2156, 2163], opt.gpu_id, opt.batch_size, part='train')
-    dev_dataset = LateFusionDataset(opt.signal_data, opt.image_data, sig_model, img_model, sig_type, img_type,
-                                      [17111, 2156, 2163], opt.gpu_id, opt.batch_size, part='dev')
-    test_dataset = LateFusionDataset(opt.signal_data, opt.image_data, sig_model, img_model, sig_type, img_type,
-                                      [17111, 2156, 2163], opt.gpu_id, opt.batch_size, part='test')
+    train_dataset = early.FusionDataset(opt.signal_data, opt.image_data, [17111, 2156, 2163], part='train')
+    dev_dataset = early.FusionDataset(opt.signal_data, opt.image_data, [17111, 2156, 2163], part='dev')
+    test_dataset = early.FusionDataset(opt.signal_data, opt.image_data, [17111, 2156, 2163], part='test')
 
     train_dataloader = DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=False)
     dev_dataloader = DataLoader(dev_dataset, batch_size=opt.batch_size, shuffle=False)
     test_dataloader = DataLoader(test_dataset, batch_size=opt.batch_size, shuffle=False)
 
-    model = LateFusionNet(4, 8, opt.hidden_size, opt.dropout).to(opt.gpu_id)
+    model = JointFusionNet(4, sig_features, img_features, opt.hidden_size, opt.dropout,
+                           sig_model, img_model).to(opt.gpu_id)
 
     # get an optimizer
     optims = {
@@ -225,8 +162,8 @@ def main():
     # and https://discuss.pytorch.org/t/multi-label-multi-class-class-imbalance/37573/2
     class_weights = torch.tensor([9528 / 5486, 9528 / 5250, 9528 / 4907, 9528 / 2655], dtype=torch.float)
     class_weights = class_weights.to(opt.gpu_id)
-    criterion = nn.BCEWithLogitsLoss(
-        pos_weight=class_weights)  # https://learnopencv.com/multi-label-image-classification-with-pytorch-image-tagging/
+    criterion = nn.BCEWithLogitsLoss(pos_weight=class_weights)
+    # https://learnopencv.com/multi-label-image-classification-with-pytorch-image-tagging/
     # https://pytorch.org/docs/stable/generated/torch.nn.BCEWithLogitsLoss.html
 
     # training loop
@@ -238,10 +175,14 @@ def main():
     train_losses = []
     for e in epochs:
         print('Training epoch {}'.format(e))
-        for i, (X_batch, y_batch) in enumerate(train_dataloader):
-            loss = gru.train_batch(
-                X_batch, y_batch, model, optimizer, criterion, gpu_id=opt.gpu_id)
-            del X_batch
+        # print(list(img_model.conv2d_1.parameters())[0][0, 0])
+        # print(list(sig_model.rnn.parameters())[0][:10])
+        for i, (X_sig_batch, X_img_batch, y_batch) in enumerate(train_dataloader):
+            print('batch {} of {}'.format(i + 1, len(train_dataloader)), end='\r')
+            loss = early.fusion_train_batch(
+                X_sig_batch, X_img_batch, y_batch, model, optimizer, criterion, gpu_id=opt.gpu_id)
+            del X_sig_batch
+            del X_img_batch
             del y_batch
             torch.cuda.empty_cache()
             train_losses.append(loss)
@@ -250,8 +191,8 @@ def main():
         print('Training loss: %.4f' % (mean_loss))
 
         train_mean_losses.append(mean_loss)
-        sensitivity, specificity = gru.evaluate(model, dev_dataloader, 'dev', gpu_id=opt.gpu_id)
-        val_loss = gru.compute_loss(model, dev_dataloader, criterion, gpu_id=opt.gpu_id)
+        sensitivity, specificity = early.fusion_evaluate(model, dev_dataloader, 'dev', gpu_id=opt.gpu_id)
+        val_loss = early.fusion_compute_loss(model, dev_dataloader, criterion, gpu_id=opt.gpu_id)
         valid_mean_losses.append(val_loss)
         valid_sensitivity.append(sensitivity)
         valid_specificity.append(specificity)
@@ -263,13 +204,13 @@ def main():
         # save the model at each epoch where the validation loss is the best so far
         if val_loss == np.min(valid_mean_losses):
             torch.save(model.state_dict(),
-                       os.path.join(opt.path_save_model, str(datetime.timestamp(dt)) + 'model' + str(e.item())))
+                       os.path.join(opt.path_save_model, str(datetime.timestamp(dt)) + 'early_lr_3max_model' + str(e.item())))
         elif sensitivity == np.max(valid_sensitivity):
             torch.save(model.state_dict(),
-                       os.path.join(opt.path_save_model, str(datetime.timestamp(dt)) + 'model_val' + str(e.item())))
+                       os.path.join(opt.path_save_model, str(datetime.timestamp(dt)) + 'early_lr_3max_model_val' + str(e.item())))
 
     # Results on test set:
-    matrix = gru.evaluate(model, test_dataloader, 'test', gpu_id=opt.gpu_id)
+    matrix = early.fusion_evaluate(model, test_dataloader, 'test', gpu_id=opt.gpu_id)
 
     # compute sensitivity and specificity for each class:
     MI_sensi = matrix[0, 0] / (matrix[0, 0] + matrix[0, 1])
@@ -294,11 +235,14 @@ def main():
 
     # plot
     plot_losses(epochs, valid_mean_losses, train_mean_losses, ylabel='Loss',
-                name='training-validation-loss-{}-{}'.format(opt.learning_rate, opt.optimizer))
+                name='training-validation-loss-joint-pre-{}-{}-{}-{}-{}-{}'.format(
+                    opt.learning_rate, opt.optimizer, opt.dropout, opt.epochs, opt.hidden_size, opt.batch_size))
     plot(epochs, valid_specificity, ylabel='Specificity',
-         name='validation-specificity-{}-{}'.format(opt.learning_rate, opt.optimizer))
+         name='validation-specificity-joint-pre-{}-{}-{}-{}-{}-{}'.format(
+             opt.learning_rate, opt.optimizer, opt.dropout, opt.epochs, opt.hidden_size, opt.batch_size))
     plot(epochs, valid_sensitivity, ylabel='Sensitivity',
-         name='validation-sensitivity-{}-{}'.format(opt.learning_rate, opt.optimizer))
+         name='validation-sensitivity-joint-pre-{}-{}-{}-{}-{}-{}'.format(
+             opt.learning_rate, opt.optimizer, opt.dropout, opt.epochs, opt.hidden_size, opt.batch_size))
 
 
 if __name__ == '__main__':
