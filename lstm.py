472285828e509ee5b2b7e69653d2a07070d3c09f
marianaagdias <marianaagdias97@gmail.com>
2022-10-12 18:02:30 +0100

    sample weights; threshold optimization (changes in functions: predict, evaluate. new function: threshold_optimization); early stop with patience; test on test set with best model (min val loss)

--- lstm.py
+++ lstm.py
@@ -14,6 +14,7 @@ from datetime import datetime
 import statistics
 import numpy as np
 import os
+from sklearn.metrics import roc_curve
 
 
 class LSTM(nn.Module):
@@ -94,17 +95,17 @@ def train_batch(X, y, model, optimizer, criterion, gpu_id=None, **kwargs):
     return loss.item()
 
 
-def predict(model, X):
+def predict(model, X, thr):
     """
     Make labels_train predictions for "X" (batch_size, 1000, 3)
     """
     logits_ = model(X)  # (batch_size, n_classes)
     probabilities = torch.sigmoid(logits_).cpu()
-    pred_labels = np.array(probabilities > 0.5, dtype=float)  # (batch_size, n_classes)
+    pred_labels = np.array(probabilities.numpy() > thr, dtype=float)  # (batch_size, n_classes)
     return pred_labels
 
 
-def evaluate(model, dataloader, part, gpu_id=None):
+def evaluate(model, dataloader, thr, gpu_id=None):
     """
     model: Pytorch model
     X (batch_size, 1000, 3) : batch of examples
@@ -116,7 +117,7 @@ def evaluate(model, dataloader, part, gpu_id=None):
         for i, (x_batch, y_batch) in enumerate(dataloader):
             print('eval {} of {}'.format(i + 1, len(dataloader)), end='\r')
             x_batch, y_batch = x_batch.to(gpu_id), y_batch.to(gpu_id)
-            y_pred = predict(model, x_batch)
+            y_pred = predict(model, x_batch, thr)
             y_true = np.array(y_batch.cpu())
             matrix = compute_scores(y_true, y_pred, matrix)
 
@@ -126,11 +127,8 @@ def evaluate(model, dataloader, part, gpu_id=None):
 
         model.train()
 
-    if part == 'dev':
-        return compute_scores_dev(matrix)
-    if part == 'test':
-        return matrix
-        # cols: TP, FN, FP, TN
+    return matrix
+    # cols: TP, FN, FP, TN
 
 
 # Validation loss
@@ -153,9 +151,40 @@ def compute_loss(model, dataloader, criterion, gpu_id=None):
         return statistics.mean(val_losses)
 
 
+def threshold_optimization(model, dataloader, gpu_id=None):
+    """
+    Make labels_train predictions for "X" (batch_size, 1000, 3)
+    """
+    model.eval()
+    with torch.no_grad():
+        threshold_opt = np.zeros(4)
+        for _, (X, Y) in enumerate(dataloader):
+            X, Y = X.to(gpu_id), Y.to(gpu_id)
+
+            Y = np.array(Y.cpu())
+            #print(Y)
+
+            logits_ = model(X)  # (batch_size, n_classes)
+            probabilities = torch.sigmoid(logits_).cpu()
+
+            # find the optimal threshold with ROC curve for each disease
+
+            for dis in range(0, 4):
+                # print(probabilities[:, dis])
+                # print(Y[:, dis])
+                fpr, tpr, thresholds = roc_curve(Y[:, dis], probabilities[:, dis])
+                # geometric mean of sensitivity and specificity
+                gmean = np.sqrt(tpr * (1 - fpr))
+                # optimal threshold
+                index = np.argmax(gmean)
+                threshold_opt[dis] = round(thresholds[index], ndigits=2)
+
+    return threshold_opt
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-data', default=None,
+    parser.add_argument('-data', default='data_for_rnn/',
                         help="Path to the dataset.")
     parser.add_argument('-epochs', default=200, type=int,
                         help="""Number of epochs to train the model.""")
@@ -167,11 +196,13 @@ def main():
     parser.add_argument('-optimizer',
                         choices=['sgd', 'adam'], default='adam')
     parser.add_argument('-gpu_id', type=int, default=None)
-    parser.add_argument('-path_save_model', default=None,
+    parser.add_argument('-path_save_model', default='save_models/',
                         help='Path to save the model')
     parser.add_argument('-num_layers', type=int, default=2)
     parser.add_argument('-hidden_size', type=int, default=128)
     parser.add_argument('-bidirectional', type=bool, default=False)
+    parser.add_argument('-early_stop', type=bool, default=True)
+    parser.add_argument('-patience', type=int, default=20)
     opt = parser.parse_args()
 
     configure_seed(seed=42)
@@ -186,6 +217,7 @@ def main():
     train_dataloader = DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True)
     dev_dataloader = DataLoader(dev_dataset, batch_size=1, shuffle=False)
     test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+    dev_dataloader_thr = DataLoader(dev_dataset, batch_size=2156, shuffle=False)
 
     input_size = 3
     hidden_size = opt.hidden_size
@@ -211,7 +243,7 @@ def main():
     # get a loss criterion and compute the class weights (nbnegative/nbpositive)
     # according to the comments https://discuss.pytorch.org/t/weighted-binary-cross-entropy/51156/6
     # and https://discuss.pytorch.org/t/multi-label-multi-class-class-imbalance/37573/2
-    class_weights = torch.tensor([9528 / 5486, 9528 / 5250, 9528 / 4907, 9528 / 2655], dtype=torch.float)
+    class_weights = torch.tensor([17111 / 4389, 17111 / 3136, 17111 / 1915, 17111 / 417], dtype=torch.float)
     class_weights = class_weights.to(opt.gpu_id)
     criterion = nn.BCEWithLogitsLoss(
         pos_weight=class_weights)  # https://learnopencv.com/multi-label-image-classification-with-pytorch-image-tagging/
@@ -221,9 +253,8 @@ def main():
     epochs = torch.arange(1, opt.epochs + 1)
     train_mean_losses = []
     valid_mean_losses = []
-    valid_specificity = []
-    valid_sensitivity = []
     train_losses = []
+    epochs_run = opt.epochs
     for ii in epochs:
         print('Training epoch {}'.format(ii))
         for i, (X_batch, y_batch) in enumerate(train_dataloader):
@@ -238,23 +269,33 @@ def main():
         print('Training loss: %.4f' % (mean_loss))
 
         train_mean_losses.append(mean_loss)
-        sensitivity, specificity = evaluate(model, dev_dataloader, 'dev', gpu_id=opt.gpu_id)
         val_loss = compute_loss(model, dev_dataloader, criterion, gpu_id=opt.gpu_id)
         valid_mean_losses.append(val_loss)
-        valid_sensitivity.append(sensitivity)
-        valid_specificity.append(specificity)
-        print('Valid specificity: %.4f' % (valid_specificity[-1]))
-        print('Valid sensitivity: %.4f' % (valid_sensitivity[-1]))
 
         dt = datetime.now()
         # https://pytorch.org/tutorials/beginner/saving_loading_models.html
         # save the model at each epoch where the validation loss is the best so far
         if val_loss == np.min(valid_mean_losses):
-            torch.save(model.state_dict(),
-                       os.path.join(opt.path_save_model, str(datetime.timestamp(dt)) + 'model' + str(ii.item())))
+            f = os.path.join(opt.path_save_model, str(datetime.timestamp(dt)) + 'model' + str(ii.item()))
+            torch.save(model.state_dict(), f)
+
+        # early stop - if validation loss does not increase for 15 epochs, stop learning process
+        if opt.early_stop:
+            if ii > opt.patience:
+                if valid_mean_losses[ii - opt.patience] == np.min(valid_mean_losses[ii - opt.patience:]):
+                    epochs_run = ii
+                    break
+
+    # Make predictions based on best model (lowest validation loss)
+    # Load model
+    model.load_state_dict(torch.load(f))
+    model.eval()
+
+    # Threshold optimization on validation set
+    thr = threshold_optimization(model, dev_dataloader_thr)
 
     # Results on test set:
-    matrix = evaluate(model, test_dataloader, 'test', gpu_id=opt.gpu_id)
+    matrix = evaluate(model, test_dataloader, thr, gpu_id=opt.gpu_id)
 
     # compute sensitivity and specificity for each class:
     MI_sensi = matrix[0, 0] / (matrix[0, 0] + matrix[0, 1])
@@ -277,13 +318,18 @@ def main():
           + '\n' + 'HYP: sensitivity - ' + str(HYP_sensi) + '; specificity - ' + str(HYP_spec)
           + '\n' + 'mean: sensitivity - ' + str(mean_sensi) + '; specificity - ' + str(mean_spec))
 
+    dt = datetime.now()
+    with open('results/' + 'model' + str(epochs_run) + '_' + str(datetime.timestamp(dt)) + '.txt', 'w') as f:
+        print('Final Test Results: \n ' + str(matrix) + '\n' + 'MI: sensitivity - ' + str(MI_sensi) + '; specificity - '
+              + str(MI_spec) + '\n' + 'STTC: sensitivity - ' + str(STTC_sensi) + '; specificity - ' + str(STTC_spec)
+              + '\n' + 'CD: sensitivity - ' + str(CD_sensi) + '; specificity - ' + str(CD_spec)
+              + '\n' + 'HYP: sensitivity - ' + str(HYP_sensi) + '; specificity - ' + str(HYP_spec)
+              + '\n' + 'mean: sensitivity - ' + str(mean_sensi) + '; specificity - ' + str(mean_spec), file=f)
+
     # plot
-    plot_losses(epochs, valid_mean_losses, train_mean_losses, ylabel='Loss',
-                name='training-validation-loss-{}-{}'.format(opt.learning_rate, opt.optimizer))
-    plot(epochs, valid_specificity, ylabel='Specificity',
-         name='validation-specificity-{}-{}'.format(opt.learning_rate, opt.optimizer))
-    plot(epochs, valid_sensitivity, ylabel='Sensitivity',
-         name='validation-sensitivity-{}-{}'.format(opt.learning_rate, opt.optimizer))
+    epochs_axis = torch.arange(1, epochs_run + 1)
+    plot_losses(epochs_axis, valid_mean_losses, train_mean_losses, ylabel='Loss',
+                name='training-validation-loss-{}-{}-{}'.format(opt.learning_rate, opt.optimizer, dt))
 
 
 if __name__ == '__main__':
