9b5a707821634dfebe7759f38d18379e9a0a10e9
marianaagdias <marianaagdias97@gmail.com>
2023-11-21 17:29:04 +0000

    threshold optimization correction; added best 1D model's weights; other minor changes

--- eval_saved_models.py
+++ eval_saved_models.py
@@ -3,10 +3,11 @@ import numpy as np
 from torch.utils.data import DataLoader
 from utils import Dataset_for_RNN, configure_device, configure_seed
 import argparse
-from lstm import LSTM, evaluate_with_norm
+from lstm import LSTM
 from cnn_lstm import CNN1d_LSTM
 from cnn_gru import CNN1d_GRU
-from gru import RNN, threshold_optimization, auroc, evaluate
+from gru_with_attention import RNN_att
+from gru import RNN, threshold_optimization, auroc, evaluate_with_norm
 
 
 def main():
@@ -48,6 +49,20 @@ def main():
         model_bigru.load_state_dict(
             torch.load('best_trained_rnns/grubi_dropout05_lr0005_model5'))
         model = model_bigru.to(opt.gpu_id)
+    elif opt.model == 'bigruattention':
+        # BiGRU
+        model_bigru_att = RNN_att(3, hidden_size=128, num_layers=2, n_classes=4, dropout_rate=0.5, gpu_id=gpu_id,
+                          bidirectional=True)
+        model_bigru_att.load_state_dict(
+            torch.load('best_trained_rnns/grubi_attention_model4'))
+        model = model_bigru_att.to(opt.gpu_id)
+    elif opt.model == 'gruattention':
+        # BiGRU
+        model_gru_att = RNN_att(3, hidden_size=128, num_layers=3, n_classes=4, dropout_rate=0, gpu_id=gpu_id,
+                                  bidirectional=False)
+        model_gru_att.load_state_dict(
+            torch.load('best_trained_rnns/gru_attention_model4'))
+        model = model_gru_att.to(opt.gpu_id)
     elif opt.model == 'cnn_gru':
         # 1D-CNN + GRU
         model_cnn_gru = CNN1d_GRU(input_size=3, hidden_size=256, n_classes=4, dropout_rate=0.3, gpu_id=gpu_id)
@@ -77,7 +92,7 @@ def main():
 
     # dev dataset
     dev_dataset = Dataset_for_RNN(path_to_data, [17111, 2156, 2163], 'dev')
-    dev_dataloader = DataLoader(dev_dataset, batch_size=2156, shuffle=False)
+    dev_dataloader = DataLoader(dev_dataset, batch_size=512, shuffle=False)
 
     # threshold optimization
     thr = threshold_optimization(model, dev_dataloader)
